2e5e5a8c5b8f4b4112a6148316760d3fd0d156d1
Alexander <alexanderwu271206@gmail.com>
2025-02-28 15:51:32 +0100

    added fade view transition

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  RouteReuseStrategy,
-  provideRouter,
-  withPreloading,
-  PreloadAllModules,
-} from '@angular/router';
-import {
-  IonicRouteStrategy,
-  provideIonicAngular,
-} from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideAnimations(),
+    provideRouter(routes, withViewTransitions()),
   ],
 });
